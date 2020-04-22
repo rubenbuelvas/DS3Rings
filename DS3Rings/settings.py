915dde8a78182592d09cb50372e2b908e0511e2e
Ruben Buelvas <rcbuelvasv@gmail.com>
2020-04-21 19:17:25 -0500

    Heruku config

--- DS3Rings/settings.py
+++ DS3Rings/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'es_us'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'America/Bogota'
 
