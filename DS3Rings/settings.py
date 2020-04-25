3f1152e6341187c0dc06ac34185ad50680a3ede0
unknown <rcbuelvasv@gmail.com>
2020-04-25 19:54:37 -0400

    Users app

--- DS3Rings/settings.py
+++ DS3Rings/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'rings.apps.RingsConfig',
+    'rings.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- DS3Rings/urls.py
+++ DS3Rings/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('rings.urls')),
+    path('', include('users.urls')),
+    path('rings/', include('rings.urls')),
     path('admin/', admin.site.urls),
 ]
