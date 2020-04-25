1c563ac4abe9e74d53bc1471e69bc40cfcefc2e4
unknown <rcbuelvasv@gmail.com>
2020-04-25 14:27:13 -0400

    Models and favicon

--- rings/models.py
+++ rings/models.py
@@ -1,7 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-"""class ring(models.model):
-    name 
-    location
-    """
+
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Ring(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.ForeignKey(Location, on_delete=models.DO_NOTHING)
+    user = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.name
