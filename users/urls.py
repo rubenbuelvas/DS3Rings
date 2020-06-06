6d1e67e03ba028a42e9a8e684ae5d116ca789c7f
unknown <rcbuelvasv@gmail.com>
2020-06-06 13:40:07 -0500

    Login form CSS fix

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.register, name='users-register'),
+    path('', views.login, name='users-login'),
+    path('register/', views.register, name='users-register')
 ]
--- users/views.py
+++ users/views.py
@@ -5,3 +5,8 @@ from django.contrib.auth.forms import UserCreationForm
 def register(request):
     form = UserCreationForm()
     return render(request, "users/register.html", {"form": form})
+
+
+def login(request):
+    form = UserCreationForm()
+    return render(request, "users/login.html", {"form": form})
