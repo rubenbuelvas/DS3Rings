900e5166e594cca349b4f7a61c00339bf1a4e113
unknown <rcbuelvasv@gmail.com>
2020-06-09 22:29:52 -0500

    If user authenticated

--- rings/views.py
+++ rings/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Ring
+
 
 # Create your views here.
 def home(request):
     context = {
         'rings': Ring.objects.all()
     }
-    return render(request, 'rings/home.html', context)
+    if request.user.is_authenticated:
+        return render(request, 'rings/home.html', context)
+    else:
+        return redirect('users-login')
